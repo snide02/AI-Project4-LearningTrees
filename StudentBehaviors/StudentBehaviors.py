6fcb6ab76381ea4f8f4ed2365c1f71dfb7307b54
snide02 <michaelsnider2002@gmail.com>
2024-04-17 14:48:43 -0400

    Imported Numpy and added lines from pdf for DaysDrink

--- StudentBehaviors/StudentBehaviors.py
+++ StudentBehaviors/StudentBehaviors.py
@@ -1,18 +1,19 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-bal_df = pd.read_csv('overdrawn.csv')
+df = pd.read_csv('overdrawn.csv')
 
 # convert categorical data to int representations of unique categories
-for col in bal_df.columns:
-    labels, uniques = pd.factorize(bal_df[col])
-    bal_df[col] = labels
+for col in df.columns:
+    labels, uniques = pd.factorize(df[col])
+    df[col] = labels
     
-X = bal_df.drop(columns='Overdrawn')
-y = bal_df['Overdrawn']
+X = df.drop(columns='Overdrawn')
+y = df['Overdrawn']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 dtree = DecisionTreeClassifier()
@@ -20,6 +21,18 @@ dtree.fit(X_train, y_train)
 predictions = dtree.predict(X_test)
 print(accuracy_score(y_test, predictions))
 print(confusion_matrix(y_test, predictions))
+
+# Change DaysDrink into categorical data
+conditions = [
+    (df['DaysDrink'] < 7),
+    (df['DaysDrink'] >= 14),
+    (df['DaysDrink'] >= 7) & (df['DaysDrink'] < 14)
+]
+
+categories = [0, 2, 1]
+
+# Apply the coniditions to create the categorical data
+df['DaysDrink'] = np.select(conditions, categories)
 
 import graphviz
 dot_data = tree.export_graphviz(dtree, out_file=None,
