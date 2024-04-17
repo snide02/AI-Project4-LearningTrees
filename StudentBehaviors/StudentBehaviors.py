50362974a85fbff9670597b579d52b6d71a2361c
snide02 <michaelsnider2002@gmail.com>
2024-04-17 17:42:02 -0400

    Warren added the Predictions stuff

--- StudentBehaviors/StudentBehaviors.py
+++ StudentBehaviors/StudentBehaviors.py
@@ -24,15 +24,26 @@ print(confusion_matrix(y_test, predictions))
 
 # Change DaysDrink into categorical data
 conditions = [
-    (df['DaysDrink'] < 7),
-    (df['DaysDrink'] >= 14),
-    (df['DaysDrink'] >= 7) & (df['DaysDrink'] < 14)
+    (df['DaysDrink'] < 7), #catergory 1
+    (df['DaysDrink'] >= 14), #catergory 2
+    (df['DaysDrink'] >= 7) & (df['DaysDrink'] < 14) #catergory 3
 ]
 
 categories = [0, 2, 1]
 
 # Apply the coniditions to create the categorical data
 df['DaysDrink'] = np.select(conditions, categories)
+
+query1 = [20, 0, 10], [25, 1, 5], [19, 0, 20], [22, 1, 15], [21, 0, 20]
+#query1 = [20, 0, 1], [25, 1, 0], [19, 0, 2], [22, 1, 2], [21, 0, 2]
+predict1 = dtree.predict_proba(query1 )
+num = 1
+for x in predict1:
+    if(x[0] > 0.5):
+        print('Prediction ' + str(num) + ': Will the student overdraw a checking account? Yes')
+    else:
+        print('Prediction ' + str(num) + ': Will the student overdraw a checking account? No')
+    num+=1
 
 import graphviz
 dot_data = tree.export_graphviz(dtree, out_file=None,
